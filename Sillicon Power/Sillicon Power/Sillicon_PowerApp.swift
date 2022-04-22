bfbd1f83dd3b64459ad14b59bfbcf0ee121fdace
Raul Romero <raulromerodev@gmail.com>
2022-04-22 16:12:11 +0200

    [Added] Project initialisation via Xcode.

---
+++ Sillicon Power/Sillicon Power/Sillicon_PowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sillicon_PowerApp.swift
+//  Sillicon Power
+//
+//  Created by Raul Romero on 22/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Sillicon_PowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sillicon Power/Sillicon PowerUITests/Sillicon_PowerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sillicon_PowerUITestsLaunchTests.swift
+//  Sillicon PowerUITests
+//
+//  Created by Raul Romero on 22/4/22.
+//
+
+import XCTest
+
+class Sillicon_PowerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
